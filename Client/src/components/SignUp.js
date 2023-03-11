c98b8667ffcb3cf4ad7f29ae6926d9ddc8453b0d
Shivani Vemula <76956616+shivanidotcom@users.noreply.github.com>
2023-03-11 16:59:12 +0530

    Update SignUp.js

--- Client/src/components/SignUp.js
+++ Client/src/components/SignUp.js
@@ -90,7 +90,7 @@ const Signup=()=>{
                 onChange={handleInputs}/>
             </div>
             <div class="input-box">
-                <input type="text" name='email' placeholder="Enter your name" required
+                <input type="text" name='email' placeholder="Enter your work" required
                 value={user.email}
                 onChange={handleInputs}/>
             </div>
